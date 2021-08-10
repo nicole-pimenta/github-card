db2e26b41b366e88dceed86ff1b5ecc21eb76d80
Nicole Pimenta <niipimenta@hotmail.com>
2021-08-10 20:28:49 -0300

    ajustes

--- src/componentes/Dashboard/index.js
+++ src/componentes/Dashboard/index.js
@@ -1,14 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Card from "../Card";
 import { FaGithub } from "react-icons/fa";
+import "./styles.css";
 
 const Dashboard = () => {
+  const [nameInput, setNameInput] = useState([]);
   const [userInput, setUserInput] = useState([]);
   const [card, setCard] = useState([]);
   const [errorMsg, setErrorMsg] = useState("");
 
   const handleUserInput = () => {
-    fetch(`https://api.github.com/repos/${userInput}`)
+    fetch(`https://api.github.com/repos/${nameInput}/${userInput}`)
       .then((response) => response.json())
       .then((response) => {
         if (response.message === "Not Found") {
@@ -17,14 +19,21 @@ const Dashboard = () => {
         setCard([...card, response]);
       });
     setErrorMsg("");
+    setNameInput("");
     setUserInput("");
     if (userInput === "") {
       setErrorMsg("Erro na busca do repositório");
     }
   };
 
+  const reset = () => {
+    setCard([]);
+  };
+
+  console.log(card.length);
+
   return (
-    <div>
+    <div className="container-dashboard">
       <h1>
         GitHub Card <FaGithub />
       </h1>
@@ -33,10 +42,11 @@ const Dashboard = () => {
       <div className="container-input">
         <input
           type="text"
-          value={userInput}
-          onChange={(event) => setUserInput(event.target.value)}
+          value={nameInput}
+          onChange={(event) => setNameInput(event.target.value)}
           placeholder="Nome do usuário"
         />
+
         <input
           type="text"
           value={userInput}
@@ -45,20 +55,25 @@ const Dashboard = () => {
         />
         <button onClick={() => handleUserInput()}>Search</button>
       </div>
-      {card.message === "Not Found" ? (
+      {card.message === "Not Found" && (
         <span className="error-msg">"Erro na busca do repositório"</span>
-      ) : null}
+      )}
       <span className="error-msg">{errorMsg}</span>
       <div className="container-card">
         {card
           .filter((ele) => ele.message !== "Not Found")
           .map((ele, index) => (
             <div key={index}>
-              {" "}
-              <Card card={ele} />{" "}
+              <Card card={ele} />
             </div>
           ))}
       </div>
+
+      {card.length !== 0 ? (
+        <button onClick={reset} className="reset">
+          Resetar
+        </button>
+      ) : null}
     </div>
   );
 };
